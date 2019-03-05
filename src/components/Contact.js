371b69d2a4c7bef030f1bfa8ae72a0155fa206d9
Angel Gomez Salazar <agomezs@fb.com>
2019-03-05 09:20:48 -0800

    Move contact links to left

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,29 @@
 import React from 'react';
 
+const Links = ({position}) => (
+  <div className={position === 'left' ? 'links-leftpane' : 'links-rightpane'}>
+    <a href="https://github.com/ags-">GH: ags-</a>
+    <br />
+    <a href="https://twitter.com/ags1_0">Twitter: @ags1_0</a>
+    <br />
+    <a href="https://www.instagram.com/ags1.0/">IG: @ags1.0</a>
+  </div>
+);
+
 export default () => (
   <div className="contact">
     <div className="contact-contents">
       <div className="leftpane">
         <h2 className="leftpane-top">Contact</h2>
+        <Links position="left" />
       </div>
       <div className="rightpane">
         <p className="rightpane-top">
           Angel Gomez Salazar is a San Francisco based Software Engineer
-          building cool stuff at Facebook. Currently on the Oculus PC Team,
-          previously GraphQL.
+          building cool stuff at Facebook. Currently on the Oculus Dev Infra team,
+          previously GraphQL and Oculus Skyline.
         </p>
-        <div className="links">
-          <a href="https://github.com/ags-">GH: ags-</a>
-          <br />
-          <a href="https://twitter.com/ags1_0">Twitter: @ags1_0</a>
-          <br />
-          <a href="https://www.instagram.com/ags1.0/">IG: @ags1.0</a>
-        </div>
+        <Links position="right" />
       </div>
     </div>
   </div>
